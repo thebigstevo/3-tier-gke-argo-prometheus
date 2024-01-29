7ae7df8687db5422e008ecca91bf6f4b5edf9ff9
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-01-29 04:27:29 +0000

    updated helm provider with cluster ca

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -22,5 +22,6 @@ provider "helm" {
   kubernetes {
     host                   = "https://${google_container_cluster.primary.k8s_cluster_endpoint}"
     token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(module.k8s_cluster.cluster_ca_certificate)
   }
 }
