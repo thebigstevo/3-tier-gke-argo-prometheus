beeb675b1923407dda143ca42f99dcb705680718
Stephen Somuah <dev@theinfoseclab.com>
2024-01-29 03:14:37 +0000

    updated outputs with child module output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,10 @@
-# output "cluster_certificate" {
-#   value = module.k8s_cluster.cluster_ca_certificate
-# }
+
+output "k8s_cluster_name" {
+  value = module.k8s_cluster.name
+}
+
+output "k8s_cluster_version" {
+  value = module.k8s_cluster.master_version
+}
+
+# Include other outputs as needed
