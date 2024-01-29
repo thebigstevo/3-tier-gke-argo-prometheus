07134a1f01826fc0eb242f2b466a020c0ea95110
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-01-29 16:24:48 +0000

    removed some code for troubleshooting

--- terraform/modules/argocd/main.tf
+++ terraform/modules/argocd/main.tf
@@ -13,7 +13,7 @@ resource "helm_release" "argocd" {
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   # namespace  = kubernetes_namespace.argocd.metadata.0.name
-  depends_on = [ kubernetes_namespace.argocd ]
+  # depends_on = [ kubernetes_namespace.argocd ]
 
 #   values = [
 #     "${file("values.yaml")}"
