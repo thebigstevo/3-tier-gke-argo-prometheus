885894b605407c2647a98859027568eb345bbf8e
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-01-29 04:10:25 +0000

    added helm provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -18,4 +18,9 @@ provider "google" {
   zone    = var.zone
 }
 
-
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.primary.k8s_cluster_endpoint}"
+    token                  = data.google_client_config.default.access_token
+  }
+}
