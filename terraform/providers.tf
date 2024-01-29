e36fa38a137edb5d21bdf2b9741486a2b2f25ac7
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-01-29 04:40:00 +0000

    corrected host value

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -20,7 +20,7 @@ provider "google" {
 
 provider "helm" {
   kubernetes {
-    host                   = "https://${module.k8s_cluster.k8s_cluster_endpoint}"
+    host                   = "https://${module.k8s_cluster.endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(module.k8s_cluster.cluster_ca_certificate)
   
