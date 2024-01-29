02c65fb56b7aa0bb5b5b37422c41c137277966ac
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-01-29 20:27:57 +0000

    added kubectl provider to create namespace

--- terraform/modules/argocd/main.tf
+++ terraform/modules/argocd/main.tf
@@ -25,3 +25,22 @@ resource "kubectl_manifest" "argo_namespace" {
 #     value = "LoadBalancer"
 #   }
 # }
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.15.0"
+    }
+    helm={
+      source = "hashicorp/helm"
+     
+    }
+ 
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+
+  }
+}
