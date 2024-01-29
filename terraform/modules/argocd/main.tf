52ee51b7bd5817a04823d653bf004645a8b6a192
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-01-29 16:32:23 +0000

    added removed argocd for further troubleshooting

--- terraform/modules/argocd/main.tf
+++ terraform/modules/argocd/main.tf
@@ -8,18 +8,18 @@ resource "kubernetes_namespace" "argocd" {
 }
 
 
-resource "helm_release" "argocd" {
-  name       = "my-redis-release"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  depends_on = [ kubernetes_namespace.argocd ]
-  namespace  = kubernetes_namespace.argocd.metadata.0.name
-#   values = [
-#     "${file("values.yaml")}"
-#   ]
+# resource "helm_release" "argocd" {
+#   name       = "my-redis-release"
+#   repository = "https://argoproj.github.io/argo-helm"
+#   chart      = "argo-cd"
+#   depends_on = [ kubernetes_namespace.argocd ]
+#   namespace  = kubernetes_namespace.argocd.metadata.0.name
+# #   values = [
+# #     "${file("values.yaml")}"
+# #   ]
 
-  set {
-    name  = "service.type"
-    value = "LoadBalancer"
-  }
-}
+#   set {
+#     name  = "service.type"
+#     value = "LoadBalancer"
+#   }
+# }
