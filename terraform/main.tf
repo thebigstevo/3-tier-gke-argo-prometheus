a8e25eceb31cad0f6b9bda4a3f9fa98216573fc6
Stephen Somuah <dev@theinfoseclab.com>
2024-01-29 01:46:52 +0000

    commented out argocd module for testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,8 @@ module "k8s_cluster" {
   source = "./modules/k8s_cluster"
 }
 
-module "argo_cd" {
-  source = "./modules/argocd"
-  depends_on = [ module.k8s_cluster ]
-}
+# module "argo_cd" {
+#   source = "./modules/argocd"
+#   depends_on = [ module.k8s_cluster ]
+# }
 
