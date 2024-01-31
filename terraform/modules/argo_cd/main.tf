490225c1576575fa976e292f9bc7f286a9816ed5
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-01-31 21:12:40 +0000

    deploy argo to default namespace

--- terraform/modules/argo_cd/main.tf
+++ terraform/modules/argo_cd/main.tf
@@ -38,8 +38,8 @@ resource "helm_release" "argocd" {
   name       = "my-redis-release"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  depends_on = [ kubernetes_namespace.argocd ]
-  namespace  = kubernetes_namespace.argocd.metadata.0.name
+  # depends_on = [ kubernetes_namespace.argocd ]
+  # namespace  = kubernetes_namespace.argocd.metadata.0.name
 #   values = [
 #     "${file("values.yaml")}"
 #   ]
