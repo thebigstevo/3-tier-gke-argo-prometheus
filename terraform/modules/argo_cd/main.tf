c6b0a8bed22e5eae597cdd1a09d102a6648c7e3d
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-01-31 21:11:09 +0000

    deploy argo to default namespace

--- terraform/modules/argo_cd/main.tf
+++ terraform/modules/argo_cd/main.tf
@@ -26,27 +26,27 @@ terraform {
 # }
 
 
-data "local_file" "argo_namespace_manifest" {
-  filename = "${path.module}/argo_namespace.yaml"
-}
-
-resource "kubectl_manifest" "argo_namespace" {
-  yaml_body = data.local_file.argo_namespace_manifest.content
-}
-
-# resource "helm_release" "argocd" {
-#   name       = "my-redis-release"
-#   repository = "https://argoproj.github.io/argo-helm"
-#   chart      = "argo-cd"
-#   depends_on = [ kubernetes_namespace.argocd ]
-#   namespace  = kubernetes_namespace.argocd.metadata.0.name
-# #   values = [
-# #     "${file("values.yaml")}"
-# #   ]
-
-#   set {
-#     name  = "service.type"
-#     value = "LoadBalancer"
-#   }
+# data "local_file" "argo_namespace_manifest" {
+#   filename = "${path.module}/argo_namespace.yaml"
 # }
+
+# resource "kubectl_manifest" "argo_namespace" {
+#   yaml_body = data.local_file.argo_namespace_manifest.content
+# }
+
+resource "helm_release" "argocd" {
+  name       = "my-redis-release"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  depends_on = [ kubernetes_namespace.argocd ]
+  namespace  = kubernetes_namespace.argocd.metadata.0.name
+#   values = [
+#     "${file("values.yaml")}"
+#   ]
+
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+}
 
