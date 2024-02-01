f9507ff19a983aa4c89ba5237c4dab5bb8742653
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-02-01 09:08:07 +0000

    installed prometheus and grafana stack

---
+++ terraform/modules/monitoring/monitoring.tf
@@ -0,0 +1,32 @@
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+
+resource "helm_release" "prometheus_stack" {
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  name       = "prometheus_grafana"
+  namespace  = "monitoring"
+  version    = "15.0.0"
+  depends_on = [ kubernetes_namespace.monitoring ]
+  set {
+     name  = "grafana.service.type"
+     value = "LoadBalancer"
+  }
+  set {
+    name  = "prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues"
+    value = "false"
+  }
+  set {
+    name  = "prometheus.prometheusSpec.serviceMonitorSelector"
+    value = "{}"
+  }
+  set {
+    name  = "prometheus.service.type"
+    value = "LoadBalancer"
+  }
+  
+}
