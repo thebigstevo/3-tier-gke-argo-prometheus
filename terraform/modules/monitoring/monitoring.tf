b24ec03e84c1dea030ee9fdd1255c70e46a2aa0a
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-02-01 18:42:23 +0000

    addedgrafana

--- terraform/modules/monitoring/monitoring.tf
+++ terraform/modules/monitoring/monitoring.tf
@@ -4,28 +4,38 @@ resource "kubernetes_namespace" "monitoring" {
   }
 }
 
-resource "helm_release" "prometheus_stack" {
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  name       = "prometheus-grafana"
-  namespace  = kubernetes_namespace.monitoring.metadata.0.name
-  version    = "15.0.0"
-  depends_on = [ kubernetes_namespace.monitoring ]
-#   set {
-#      name  = "grafana.service.type"
-#      value = "LoadBalancer"
-#   }
-#   set {
-#     name  = "prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues"
-#     value = "false"
-#   }
-#   set {
-#     name  = "prometheus.prometheusSpec.serviceMonitorSelector"
-#     value = "{}"
-#   }
-#   set {
-#     name  = "prometheus.service.type"
-#     value = "LoadBalancer"
-#   }
+# resource "helm_release" "prometheus_stack" {
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "kube-prometheus-stack"
+#   name       = "prometheus-grafana"
+#   namespace  = kubernetes_namespace.monitoring.metadata.0.name
+#   version    = "15.0.0"
+#   depends_on = [ kubernetes_namespace.monitoring ]
+# #   set {
+# #      name  = "grafana.service.type"
+# #      value = "LoadBalancer"
+# #   }
+# #   set {
+# #     name  = "prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues"
+# #     value = "false"
+# #   }
+# #   set {
+# #     name  = "prometheus.prometheusSpec.serviceMonitorSelector"
+# #     value = "{}"
+# #   }
+# #   set {
+# #     name  = "prometheus.service.type"
+# #     value = "LoadBalancer"
+# #   }
   
+# }
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace  = kubernetes_namespace.monitoring.metadata.0.name
+#   version    = "6.29.1"
+  wait       = "false"
+#   values     = ["${file("values.yaml")}"]
 }
