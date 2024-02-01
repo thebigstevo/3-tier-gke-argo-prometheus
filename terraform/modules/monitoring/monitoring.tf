08cd01e1c5d976c5014317fcbfda19726999e1d2
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-02-01 18:08:46 +0000

    removed values configuration from prometheus resource

--- terraform/modules/monitoring/monitoring.tf
+++ terraform/modules/monitoring/monitoring.tf
@@ -11,21 +11,21 @@ resource "helm_release" "prometheus_stack" {
   namespace  = kubernetes_namespace.monitoring.metadata.0.name
   version    = "15.0.0"
   depends_on = [ kubernetes_namespace.monitoring ]
-  set {
-     name  = "grafana.service.type"
-     value = "LoadBalancer"
-  }
-  set {
-    name  = "prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues"
-    value = "false"
-  }
-  set {
-    name  = "prometheus.prometheusSpec.serviceMonitorSelector"
-    value = "{}"
-  }
-  set {
-    name  = "prometheus.service.type"
-    value = "LoadBalancer"
-  }
+#   set {
+#      name  = "grafana.service.type"
+#      value = "LoadBalancer"
+#   }
+#   set {
+#     name  = "prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues"
+#     value = "false"
+#   }
+#   set {
+#     name  = "prometheus.prometheusSpec.serviceMonitorSelector"
+#     value = "{}"
+#   }
+#   set {
+#     name  = "prometheus.service.type"
+#     value = "LoadBalancer"
+#   }
   
 }
