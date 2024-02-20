1f3b1c5f98ed0dc292d21fa9d8e9423ee2517f39
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-02-20 18:41:36 +0000

    reduced disk size to 10 per node

--- terraform/modules/k8s_cluster/main.tf
+++ terraform/modules/k8s_cluster/main.tf
@@ -2,7 +2,7 @@
 resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
   location = var.region
-  network  = google_compute_network.k8s_vpc.name
+  network  = google_compute_network.k8s_vpc.namlie
   subnetwork = google_compute_subnetwork.k8s-subnet.name
 
   remove_default_node_pool = true
@@ -19,7 +19,7 @@ resource "google_container_node_pool" "primary_nodes" {
   node_config {
     preemptible  = true
     machine_type = "e2-medium"
-    disk_size_gb = 20 
+    disk_size_gb = 10
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     # service_account = google_service_account.default.email
