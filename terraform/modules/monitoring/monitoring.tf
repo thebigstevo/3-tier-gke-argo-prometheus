005daaedc22f8ce1c78b5adf74533af4b573392a
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-02-21 23:48:26 +0000

    changed helm to manifest for prometheus

--- terraform/modules/monitoring/monitoring.tf
+++ terraform/modules/monitoring/monitoring.tf
@@ -4,18 +4,24 @@ resource "kubernetes_namespace" "monitoring" {
   }
 }
 
-resource "helm_release" "prometheus" {
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus"
-  name       = "prometheus"
-  namespace  = kubernetes_namespace.monitoring.metadata.0.name
-  version    = "25.13.0"
-  depends_on = [ kubernetes_namespace.monitoring ]
-  set {
-    name  = "prometheus.service.type"
-    value = "LoadBalancer"
-  }
+# resource "helm_release" "prometheus" {
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "prometheus"
+#   name       = "prometheus"
+#   namespace  = kubernetes_namespace.monitoring.metadata.0.name
+#   version    = "25.13.0"
+#   depends_on = [ kubernetes_namespace.monitoring ]
+#   set {
+#     name  = "prometheus.service.type"
+#     value = "LoadBalancer"
+#   }
   
+# }
+
+resource "kubernetes_manifest" "prometheus"{
+ manifest = file("${path.module}/prometheus.yaml")
+
+
 }
 # #   set {
 # #      name  = "grafana.service.type"
@@ -37,12 +43,12 @@ resource "helm_release" "prometheus" {
 # }
 
 
-resource "helm_release" "grafana" {
-  name       = "grafana"
-  repository = "https://grafana.github.io/helm-charts"
-  chart      = "grafana"
-  namespace  = kubernetes_namespace.monitoring.metadata.0.name
-#   version    = "6.29.1"
-  wait       = "false"
-#   values     = ["${file("values.yaml")}"]
-}
+# resource "helm_release" "grafana" {
+#   name       = "grafana"
+#   repository = "https://grafana.github.io/helm-charts"
+#   chart      = "grafana"
+#   namespace  = kubernetes_namespace.monitoring.metadata.0.name
+# #   version    = "6.29.1"
+#   wait       = "false"
+# #   values     = ["${file("values.yaml")}"]
+# }
