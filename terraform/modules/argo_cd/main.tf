8348bef7c4b70a76be8c87535bcce30dafb824d9
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-02-22 07:11:57 +0000

    reverted argocd namespace code

--- terraform/modules/argo_cd/main.tf
+++ terraform/modules/argo_cd/main.tf
@@ -1,29 +1,13 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.15.0"
-    }
-    helm={
-      source = "hashicorp/helm"
-     
-    }
- 
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
 
-  }
-}
 ##############################################################################
 ### Deployment of the argo namespace ###
 ###############################################################################
 resource "kubernetes_namespace" "argocd" {
   metadata {
-    name = "argocd"
+    name = var.namespace_name
   }
 }
+
 
 resource "helm_release" "argocd" {
   name       = var.helm_release_name
