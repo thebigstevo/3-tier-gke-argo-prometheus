0cf856c259579e7c08e90ce16fb1ae650cdaa776
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-02-22 01:05:20 +0000

    changed variable version name

--- terraform/modules/argo_cd/main.tf
+++ terraform/modules/argo_cd/main.tf
@@ -13,7 +13,7 @@ resource "helm_release" "argocd" {
   name       = var.helm_release_name
   repository = var.repository
   chart      = var.chart
-  version = var.version
+  version = var.release_version
   depends_on = [ kubernetes_namespace.argocd ]
   namespace  = kubernetes_namespace.argocd.metadata.0.name
   values     = [file("${path.module}/manifests/values.yaml")]
