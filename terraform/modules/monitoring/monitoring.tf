9e61bf95d07c3346b3854b2458ebaa43e0bfc798
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-02-22 01:09:22 +0000

    changed variable version name

--- terraform/modules/monitoring/monitoring.tf
+++ terraform/modules/monitoring/monitoring.tf
@@ -6,15 +6,11 @@ resource "kubernetes_namespace" "monitoring" {
 
 resource "helm_release" "prometheus_stack" {
    name       = "triggermesh-prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  namespace = kubernetes_namespace.monitoring.id
-  version    = "56.8.2"
-  values     = [file("${path.module}/manifests/values.yaml")]
-  depends_on = [ kubernetes_namespace.monitoring ]
-  set {
-    name  = "prometheus.service.type"
-    value = "LoadBalancer"
-  }
-  
+   repository = "https://prometheus-community.github.io/helm-charts"
+   chart      = "kube-prometheus-stack"
+   namespace  = kubernetes_namespace.monitoring.metadata.0.name
+   version    = "56.8.2"
+   values     = [file("${path.module}/manifests/values.yaml")]
+   depends_on = [ kubernetes_namespace.monitoring ]
+
 }
