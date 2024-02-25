84a378d22c3bb891b13fea57560cff9119edd99f
Stephen Somuah <stephen.somuah@bigspark.dev>
2024-02-25 13:30:06 +0000

    updated variables

--- terraform/modules/monitoring/variables.tf
+++ terraform/modules/monitoring/variables.tf
@@ -1,3 +1,19 @@
 variable "helm_release_name" {
   default = "prometheus_stack"
 }
+
+variable "release_version" {
+  default = "56.8.2"
+}
+
+variable "chart" {
+  default = "kube-prometheus-stack"
+}
+
+variable "repository" {
+  default = "https://prometheus-community.github.io/helm-charts"
+}
+
+variable "namespace_name" {
+  default = "monitoring"
+}
